74c46b5ec6f47ca0115aa74a798ea3436e14fa9d
christianhussmann <54892854+christianhussmann@users.noreply.github.com>
2021-03-08 11:31:29 +0100

    Update HmmBot.java

--- src/dk/easv/bll/bot/HmmBot.java
+++ src/dk/easv/bll/bot/HmmBot.java
@@ -5,7 +5,7 @@ public class HmmBot extends LocalPrioritisedListBot {
 
     public HmmBot() {
         int[][] pref = {
-                {1, 2}, {0, 0}, {2, 0}, {2, 2},
+                {0, 2}, {0, 0}, {2, 0}, {2, 2},
                 {1, 1}, {0, 1}, {1, 2}, {1, 0},
                 {2, 1}};
         super.preferredMoves = pref;
